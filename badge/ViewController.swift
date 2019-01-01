74ee179de8c8dd6c4965af55fa34d45470d78ea0
Kamal <kamalchibrani@kamals-macbook-air.local>
2019-01-01 09:26:26 +0530

    initial commit

--- badge/ViewController.swift
+++ badge/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import Pods_badge
+//import BadgeSwift
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        navigationItem.rightBarButtonItem = barBadge
+//        barBadge.badgeValue = "10"
+        let button = BadgeBarButtonItem(image: "ic_call",  target: self, action: #selector(setter: barBadge))
+            let button3 = BadgeBarButtonItem(image: #imageLiteral(resourceName: "ic_notifications_white"),  target: self, action: #selector(setter: barBadge))
+   
+        button?.tintColor = UIColor.white
+        let button2 = BadgeBarButtonItem(image: "ic_call",  target: self, action: #selector(setter: self.barBadge))
+barBadge.tintColor = .white
+        navigationItem.rightBarButtonItems = [button,button2,button3] as? [UIBarButtonItem]
+       
+        button2?.badgeText = "10"
+        button3 .badgeText = "10"
+
+        
+//
+//
+button?.badgeText = "2"
+        
+    
+        
     }
 
 
+    @IBOutlet weak var barBadge: UIBarButtonItem!
+    
+
+    
+    
 }
 
